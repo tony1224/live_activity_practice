6122d30944d7601e50800dddb2cfc8fa07d1865e
jun.morita <jun.morita@gree.net>
2022-10-24 13:12:46 +0900

    create project file

---
+++ LiveActivitySample/LiveActivitySample/LiveActivitySampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitySampleApp.swift
+//  LiveActivitySample
+//
+//  Created by Jun Morita on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitySampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
